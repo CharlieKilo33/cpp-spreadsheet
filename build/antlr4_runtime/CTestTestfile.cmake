0e10b0614af0c99d181e5d5afbfd93c1cfe30e01
MIKE <mishak04@yandex.ru>
2024-10-22 04:31:48 +0300

    fix build x2

--- build/antlr4_runtime/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/projects/yandex/spreadsheet/antlr4_runtime
-# Build directory: /home/user/projects/yandex/build/antlr4_runtime
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("runtime")
